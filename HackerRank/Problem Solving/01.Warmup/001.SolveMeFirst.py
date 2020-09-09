bb0b8e4c49c2644e08ff1acc60ba358681ef9aa9
Darpan Jain <darpan.pmun@gmail.com>
2020-09-09 18:10:15 +0530

    Created Script

---
+++ HackerRank/Problem Solving/01.Warmup/001.SolveMeFirst.py
@@ -0,0 +1,13 @@
+'''
+Question : Compute the sum of two integers.
+
+Link : https://www.hackerrank.com/challenges/solve-me-first/problem
+'''
+
+def solveMeFirst(a,b):
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
