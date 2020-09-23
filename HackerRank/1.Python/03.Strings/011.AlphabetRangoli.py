b503cdd0b09efe2d3ee9d2d32816b95e3071aee8
Darpan Jain <darpan.pmun@gmail.com>
2020-09-23 14:09:27 +0530

    scripts were added with few updates

---
+++ HackerRank/1.Python/03.Strings/011.AlphabetRangoli.py
@@ -0,0 +1,25 @@
+'''
+Question : You are given an integer, N. 
+           Your task is to print an alphabet rangoli of size N.
+           size 3
+           ----c----
+           --c-b-c--
+           c-b-a-b-c
+           --c-b-c--
+           ----c----
+           
+Link : https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+'''
+
+import string
+
+def print_rangoli(size):
+    width = 4 * size - 3
+    alpha = string.ascii_lowercase
+    
+    for i in list(range(size))[::-1] + list(range(1 , size)):
+        print('-'.join(alpha[size-1 : i : -1] + alpha[i : size]).center(width , '-'))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
