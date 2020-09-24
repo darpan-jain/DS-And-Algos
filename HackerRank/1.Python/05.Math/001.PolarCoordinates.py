a7854bc18dd70a70446f8704e81abbe145543957
Darpan Jain <darpan.pmun@gmail.com>
2020-09-24 18:18:04 +0530

    Create 001.PolarCoordinates.py

---
+++ HackerRank/1.Python/05.Math/001.PolarCoordinates.py
@@ -0,0 +1,11 @@
+'''
+Question : You are given a complex z. Your task is to convert it to polar coordinates.
+
+Link : https://www.hackerrank.com/challenges/polar-coordinates/problem
+'''
+
+import cmath
+
+z = complex(input())
+print(abs(z))
+print(cmath.phase(z))
